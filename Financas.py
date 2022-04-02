749bf0a066e5598843159b8a36602a39ad17161c
Thiago Coelho <tcoelho.econ@gmail.com>
2022-04-02 11:25:20 -0300

    Formatação

--- Financas.py
+++ Financas.py
@@ -2,6 +2,7 @@ import pandas as pd
 import yfinance as yf
 import numpy as np
 
+
 def Ibovespa():
     """
     Retorno Dataframe com as empresas presentes no índice IBOVESPA
@@ -11,13 +12,12 @@ def Ibovespa():
     df.set_index("Código", inplace=True)
     return df
 
+
 class Carteira:
-    
     def __init__(self, Ativos, Pesos=None):
         self.Ativos = [Ativo + ".SA" for Ativo in Ativos]
         self.Pesos = [(1 / len(Ativos)) for i in Ativos] if Pesos is None else Pesos
         Carteira.Coletar_Precos(self)
-
 
     def Coletar_Precos(self):
         """
@@ -34,21 +34,21 @@ class Carteira:
         df.columns = self.Ativos
         self.Precos = df
         self.Tickers = tickers
-    
+
     def Desvios(self):
-        """ 
+        """
         Função para calcular rapidamente o desvio padrão de ativos de uma carteria teórica
         """
         print(" Os desvios padrão são:")
         print(self.Precos.std())
-    
+
     def Correlacao(self):
         """
         Calcular a correlação entre os ativos de uma carteira teórica
         """
         print("A Matriz de correlação entre os ativos é:")
         print(self.Precos.corr())
-    
+
     def Betas(self):
         """
         Coletar o Beta de cada ativo
@@ -60,35 +60,30 @@ class Carteira:
         print("Os betas dos ativos são:")
         print(Betas)
 
-    def Grafico(self,inicio=None, final=None):
+    def Grafico(self, inicio=None, final=None):
         """
         Gráfico com a evolução dos ativos selecionados. Se nada for passado para as datas, serão utilizados os dados
         mais recentes (no caso do final) ou mais antigos (no caso do inicio). Formato das datas:
         Ex: Grafico("2010", "2020")
-        """ 
+        """
         inicio = self.Precos.index[0] if inicio is None else inicio
         final = self.Precos.index[-1] if final is None else final
 
-        self.Precos.loc[inicio:final,:].plot()
+        self.Precos.loc[inicio:final, :].plot()
 
-    def Retornos(self,inicio=None, final=None):
+    def Retornos(self, inicio=None, final=None):
         """
         Calcula os retornos da carteira de acordo com o recorte temporal selecionado
         """
         inicio = self.Precos.index[0] if inicio is None else inicio
         final = self.Precos.index[-1] if final is None else final
 
-        Retornos = self.Precos.loc[inicio:final,:].pct_change()
+        Retornos = self.Precos.loc[inicio:final, :].pct_change()
         Retornos_Com_Pesos = Retornos.dot(np.array(self.Pesos))
-        Retornos_Acumulados = (1+Retornos_Com_Pesos).cumprod()
-        Retorno_Total = ((Retornos_Acumulados[-1] - 1)*100)
+        Retornos_Acumulados = (1 + Retornos_Com_Pesos).cumprod()
+        Retorno_Total = (Retornos_Acumulados[-1] - 1) * 100
         print(f"{Retorno_Total}%")
-
-
 
     # TODO def Modigliani(self):
 
     # TODO def Sharpe(self):
-        
-
-
